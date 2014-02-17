afd085a9277fd3ef896bb18443c46a14492afca9
delucas <videla.lucas@gmail.com>
2014-02-17 01:08:19 -0200

    Update Mano.java

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -11,7 +11,6 @@ public class Mano {
 	 * @param forma, la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
-		throw new RuntimeException("No implementado aún");
 	}
 
 	/**
@@ -21,7 +20,7 @@ public class Mano {
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		throw new RuntimeException("No implementado aún");
+		return Resultado.GANA;
 	}
 
 }
